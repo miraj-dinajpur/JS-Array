1601e536a670bc8df126619912e47ae887fe2c13
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-30 20:26:32 +0600

    Loop.js added

---
+++ loop.js
@@ -0,0 +1,15 @@
+const numbers = [87, 54, 12, 63, 41];
+// for of loop
+for(const number of numbers){
+    console.log('current item is: ', number);
+}
+
+const players = ['jamir', 'khamir', 'namir', 'damir' ]
+
+for(const p of players){
+    console.log(p);
+}
+
+for(const item of numbers){
+    console.log(item)
+}
