31fab5edc7a857b9f82445c3de17c32763cd6191
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-30 20:30:22 +0600

    slice.js added

---
+++ slice.js
@@ -0,0 +1,9 @@
+const numbers = [1, 3, 5, 6, 2, 4, 5];
+
+const slice = numbers.slice(2, 6)
+// console.log(slice);
+// console.log(numbers);
+
+const parts = numbers.splice(2, 2, 99, 888, 999, 6666);
+console.log(parts);
+console.log(numbers);
