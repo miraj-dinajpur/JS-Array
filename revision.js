fa14017807129376729c5794ae057e468abbf793
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-30 20:29:20 +0600

    revision.js added

---
+++ revision.js
@@ -0,0 +1,20 @@
+/**
+ * 1. Variable
+ * 2. conditionals
+ * 3. loop: for, while, do while
+*/
+
+const price = 50;
+const studentName = 'Jobbbar khan';
+const isRich = true;
+
+if(studentName === 'talukdar'){
+
+}
+else {
+
+}
+
+for(let i = 100; i < 125; i++){
+    
+}
