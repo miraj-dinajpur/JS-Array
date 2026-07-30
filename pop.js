0645fa704c61aaa8011cd50da65b44da858f840d
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-30 20:27:28 +0600

    pop.js added

---
+++ pop.js
@@ -0,0 +1,7 @@
+const numbers = [12, 45, 145, 25, 78, 89, 94]
+console.log(numbers);
+const firstOut = numbers.pop();
+console.log(numbers)
+const secondOut = numbers.pop();
+console.log(numbers)
+console.log(firstOut, secondOut);
