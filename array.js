c1c7aee535698d0fbfe03230e1461459600dc3e0
miraj-dinajpur <miraj.dinajpur@gmail.com>
2026-07-30 20:14:04 +0600

    array.js added

---
+++ array.js
@@ -0,0 +1,13 @@
+// variable can hold only one value. one data
+const player1 = 45;
+const player2 = 98;
+const player3 = 56;
+const player4 = 45;
+const player5 = 98;
+const player6 = 56;
+
+
+// array can hold more than one value
+const team = [123, 89, 45, 69, 78]
+const players = ['jamir', 'khamir', 'namir', 'damir' ]
+const richStudents = [true, false, true, false, false, false]
